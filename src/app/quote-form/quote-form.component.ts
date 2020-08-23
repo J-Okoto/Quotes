608aebc72ffbd0b5e8496b28afee0624cb0460f2
J-Okoto <okotojr@gmail.com>
2020-08-23 19:08:41 +0300

    added form control features

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteFormComponent implements OnInit {
 
+  newQuote = new Quote("", "", "","" ,new Date(),0,0);
   constructor() { }
 
   ngOnInit(): void {
