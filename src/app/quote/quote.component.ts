d94a56902589e5670e8552eee4b2f275754e6f82
J-Okoto <okotojr@gmail.com>
2020-08-24 16:26:56 +0300

    created highlight directive component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,14 +9,13 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {  
   quotes: Quote[] = [
 
- new Quote('Success', 'Success usually comes to those who are too busy to be looking for it.', 'Henry David Thoreau','Joe', new Date(2019, 8 - 1, 23),0,0),
-  new Quote('Failure ', 'Giving up is the only sure way to fail.', 'Gena Showalter','Joe', new Date(2019, 8 - 1, 15),0,0),
+ new Quote('Success', 'Success usually comes to those who are too busy to be looking for it.', 'Joe','Henry David Thoreau', new Date(2019, 8 - 1, 23),0,0),
+  new Quote('Failure ', 'Giving up is the only sure way to fail.', 'Gena Showalter', 'Joe',new Date(2019, 8 - 1, 15),0,0),
   new Quote('Fear', 'Everything you want is on the other side of fear.', 'Jack Canfield','Joe', new Date(2019, 8 - 1, 12),0,0),
   new Quote('Wisdom', 'Knowing yourself is the beginning of all wisdom.', 'Aristotle', 'Joe',new Date(2019, 8 - 1, 10),0,0),
   new Quote('Passion ', 'Passion is the genesis of genius.', 'Tony Robbins','Joe', new Date(2019, 8 - 1, 19),0,0),
   new Quote('Investment ', 'Never invest in a business you cannot understand.', 'Warren Buffett','Joe', new Date(2019, 8 - 1, 17),0,0)
 ];
-
 toggleDetails(index) {
   this.quotes[index].showCompleteDate = !this.quotes[index].showCompleteDate;
 }
